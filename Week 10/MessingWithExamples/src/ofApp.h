1998a6a71fb853a3f23bff641a25d32bd2635445
David Utt <davidutt36@gmail.com>
2015-11-09 15:44:56 -0500

    HW for Week 10
    Combination of Particles and the ability to change the color blending
    With different key cases inputs.
    
    1-5 changes particle swarming
    
    6-9 changes the current blending options

---
+++ Week 10/MessingWithExamples/src/ofApp.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ofMain.h"
+#include "demoParticle.h"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void resetParticles();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    particleMode currentMode;
+    string currentModeStr;
+    
+    vector <demoParticle> p;
+    vector <ofPoint> attractPoints;
+    vector <ofPoint> attractPointsWithMovement;
+    
+    ofBlendMode blendMode;
+    ofImage rainbow;
+    float           alpha;
+    float 			counter;
+    ofTrueTypeFont 	vagRounded;
+    char eventString[255];
+    char timeString[255];
+};
